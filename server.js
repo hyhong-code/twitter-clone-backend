12108110a9a6c29cf1434b6b9cc228f9318665bd
Haiyang Hong <haiyanghong@Haiyangs-iMac.local>
2020-06-21 11:24:27 -0700

    setup express

---
+++ server.js
@@ -0,0 +1,11 @@
+require('dotenv').config({ path: `{__dirname}/config/config.env` });
+const connectDB = requrie('./config/db.js');
+const app = require('./app');
+
+connectDB();
+
+const port = process.env.PORT || 5000;
+app.listen(
+  port,
+  console.log(`Server up on port ${port} in ${process.env.NODE_ENV} mode`)
+);
