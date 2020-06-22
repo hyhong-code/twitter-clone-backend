b2dac44d4acc74f10dc458662b6d274625594955
Haiyang Hong <haiyanghong@Haiyangs-iMac.local>
2020-06-21 21:22:47 -0700

    add get user route

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,6 +3,9 @@ const CustomError = require('../utils/customError');
 const User = require('../models/User');
 const QueryOptions = require('../utils/queryOptions');
 
+// @desc    Get all users
+// @route   GET /api/v1/users
+// @access  Private - "admin"
 exports.getUsers = asyncHandler(async (req, res, next) => {
   const query = User.find();
   const users = await new QueryOptions(query, req.query)
@@ -19,5 +22,22 @@ exports.getUsers = asyncHandler(async (req, res, next) => {
     status: 'success',
     results: users.length,
     data: { users },
+  });
+});
+
+// @desc    Get a user
+// @route   GET /api/v1/user/:id
+// @access  Private - "admin"
+exports.getUser = asyncHandler(async (req, res, next) => {
+  const user = await User.findById(req.params.id);
+
+  // HANDLE USER NOT EXIST
+  if (!user) {
+    return next(new CustomError(`No user with id ${req.params.id}`, 404));
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: { user },
   });
 });
--- routes/userRouter.js.js
+++ routes/userRouter.js.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const { protect } = require('../controllers/authController');
-const { getUsers } = require('../controllers/userController');
+const { getUsers, getUser } = require('../controllers/userController');
 
 const router = express.Router();
 
 router.use(protect);
 
 router.route('/').get(getUsers);
+router.route('/:id').get(getUser);
 
 module.exports = router;
