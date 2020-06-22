411e884bb8c2f46ffb8a5b32f86f3cecbddce2a7
Haiyang Hong <haiyanghong@Haiyangs-iMac.local>
2020-06-21 20:17:24 -0700

    add deleteComment route and handler

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -2,6 +2,7 @@ const asyncHandler = require('../utils/asyncHandler');
 const CustomError = require('../utils/customError');
 const Tweet = require('../models/Tweet');
 const Comment = require('../models/Comment');
+const QueryOptions = require('../utils/queryOptions');
 
 // @desc    Create a comment
 // @route   POST /api/v1/tweets/:tweetId/comments
@@ -28,22 +29,46 @@ exports.createComment = asyncHandler(async (req, res, next) => {
 });
 
 // @desc    Get all comments
-// @route   GET /api/v1/tweets/:tweetId/comments
+// @route   GET /api/comments
 // @access  Public
 exports.getComments = asyncHandler(async (req, res, next) => {
-  const tweet = await Tweet.findById(req.params.tweetId);
+  const query = Comment.find();
+  const comments = await new QueryOptions(query, req.query)
+    .filter()
+    .sort()
+    .limitFields()
+    .paginate().query;
 
-  // HANDLE TWEET DOES NOT EXIST
-  if (!tweet) {
+  res.status(200).json({
+    status: 'success',
+    data: { comments },
+  });
+});
+
+// @desc    Delete a comment
+// @route   DELETE /api/v1/comments/:id
+// @access  Public
+exports.deleteComment = asyncHandler(async (req, res, next) => {
+  const comment = await Comment.findById(req.params.id);
+
+  // HANDLE COMMENT NOT FOUND
+  if (!comment) {
     return next(
-      new CustomError(`No such tweet with id ${req.params.tweetId}`, 404)
+      new CustomError(`No comment found with id ${req.params.id}`, 404)
     );
   }
 
-  const comments = await Comment.find({ tweet: req.params.tweetId });
-  res.status(200).json({
+  // HANDLE USER IS NOT COMMENT OWNER
+  if (comment.user.toString() !== req.user.id && req.user.role !== 'admin') {
+    return next(
+      new CustomError(`User not authorized to access this route`, 401)
+    );
+  }
+
+  await comment.remove();
+
+  res.status(204).json({
     status: 'success',
-    results: comments.length,
-    data: { comments },
+    data: null,
   });
 });
--- routes/commentRouter.js
+++ routes/commentRouter.js
@@ -3,10 +3,12 @@ const { protect } = require('../controllers/authController');
 const {
   createComment,
   getComments,
+  deleteComment,
 } = require('../controllers/commentController');
 
 const router = express.Router({ mergeParams: true });
 
 router.route('/').get(getComments).post(protect, createComment);
+router.route('/:id').delete(protect, deleteComment);
 
 module.exports = router;
