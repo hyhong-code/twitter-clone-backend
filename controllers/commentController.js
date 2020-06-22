147f650de57888b75f64e8a5f3089363b06755af
Haiyang Hong <haiyanghong@Haiyangs-iMac.local>
2020-06-21 19:57:43 -0700

    add getComments route and handler

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -4,7 +4,7 @@ const Tweet = require('../models/Tweet');
 const Comment = require('../models/Comment');
 
 // @desc    Create a comment
-// @route   POST /api/v1/tweets/:tweetId/comment
+// @route   POST /api/v1/tweets/:tweetId/comments
 // @access  Private - "user", "admin"
 exports.createComment = asyncHandler(async (req, res, next) => {
   const tweet = await Tweet.findById(req.params.tweetId);
@@ -24,5 +24,26 @@ exports.createComment = asyncHandler(async (req, res, next) => {
   res.status(201).json({
     status: 'success',
     data: { comment },
+  });
+});
+
+// @desc    Get all comments
+// @route   GET /api/v1/tweets/:tweetId/comments
+// @access  Public
+exports.getComments = asyncHandler(async (req, res, next) => {
+  const tweet = await Tweet.findById(req.params.tweetId);
+
+  // HANDLE TWEET DOES NOT EXIST
+  if (!tweet) {
+    return next(
+      new CustomError(`No such tweet with id ${req.params.tweetId}`, 404)
+    );
+  }
+
+  const comments = await Comment.find({ tweet: req.params.tweetId });
+  res.status(200).json({
+    status: 'success',
+    results: comments.length,
+    data: { comments },
   });
 });
--- routes/commentRouter.js
+++ routes/commentRouter.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const { protect } = require('../controllers/authController');
-const { createComment } = require('../controllers/commentController');
+const {
+  createComment,
+  getComments,
+} = require('../controllers/commentController');
 
 const router = express.Router({ mergeParams: true });
 
-router.route('/').post(protect, createComment);
+router.route('/').get(getComments).post(protect, createComment);
 
 module.exports = router;
